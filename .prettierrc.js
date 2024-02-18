63a1b71092a6ac9ba22aa67852a25593c2630332
Daryl <daryl@datametal.io>
2024-02-18 01:44:50 -0500

    feat: ad prettier and update eslint

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'always',
+  trailingComma: 'all',
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  useTabs: false,
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+
+}
